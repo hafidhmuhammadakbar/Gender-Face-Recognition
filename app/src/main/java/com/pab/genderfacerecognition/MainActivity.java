e9c654c85881c4cf48d9af1a0361efc8ace2dca7
hafidhmuhammadakbar <hafidhmuhammadakbar1@student.uns.ac.id>
2023-07-06 16:49:44 +0700

    Edit Main UI

--- app/src/main/java/com/pab/genderfacerecognition/MainActivity.java
+++ app/src/main/java/com/pab/genderfacerecognition/MainActivity.java
@@ -10,28 +10,28 @@ import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imageView;
+//    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = findViewById(R.id.imageView);
-
-        // Create a reference to the image file you want to retrieve
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageRef = storage.getReference();
-        StorageReference imageRef = storageRef.child("4fe6097f646b518a543b9f99ff422147.jpg");
-        System.out.println(imageRef.getDownloadUrl());
-
-        imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
-            String imageUrl = uri.toString();
-            Picasso.get()
-                    .load(imageUrl)
-                    .into(imageView);
-        }).addOnFailureListener(e -> {
-            // Handle any errors that occur while fetching the download URL
-        });
+//        imageView = findViewById(R.id.imageView);
+//
+//        // Create a reference to the image file you want to retrieve
+//        FirebaseStorage storage = FirebaseStorage.getInstance();
+//        StorageReference storageRef = storage.getReference();
+//        StorageReference imageRef = storageRef.child("4fe6097f646b518a543b9f99ff422147.jpg");
+//        System.out.println(imageRef.getDownloadUrl());
+//
+//        imageRef.getDownloadUrl().addOnSuccessListener(uri -> {
+//            String imageUrl = uri.toString();
+//            Picasso.get()
+//                    .load(imageUrl)
+//                    .into(imageView);
+//        }).addOnFailureListener(e -> {
+//            // Handle any errors that occur while fetching the download URL
+//        });
     }
 }
